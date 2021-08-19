9bab95e93e6e4ff01d549266c96762b477814d5a
Tablieh <waledo.1997@hotmail.com>
2021-08-19 16:55:08 +0200

    bom

--- src/Controller/GallaryController.php
+++ src/Controller/GallaryController.php
@@ -17,9 +17,37 @@ class GallaryController extends AbstractController
         $voiture = $this->getDoctrine()
                 ->getRepository(Voiture::class)
                 ->findAll();
+        $resultat = $this->getDoctrine()
+                ->getRepository(Voiture::class)
+                ->findBy([], ["modele" => "ASC"]
+                    );
     
         return $this->render('gallary/index.html.twig', [
             'voitures' => $voiture,
+            'resultat' => $resultat,
+            
         ]);
+    }
+    /**
+     * @Route("/Voiture/{id}", name="Voiture_show", methods="GET")
+     */
+    public function showVoiture(Voiture $resultat): Response {
+        return $this->render('gallary/showVoiture.html.twig', [
+            'resultat' => $resultat
+        ]);
+    }
+    /**
+     * @Route("/del/{id}", name="Voiture_del")
+     */
+    public function del_edit(Voiture $Voiture)
+    {
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+
+        $entityManager->remove($Voiture);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('gallary');
     }
 }
