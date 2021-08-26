e7f22745aea818f94c5d935dba72186dfd234678
Tablieh <waledo.1997@hotmail.com>
2021-08-26 11:53:43 +0200

    news

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Avis;
 use App\Entity\Marque;
 use App\Entity\Modele;
 use App\Entity\Voiture;
+use App\Repository\AvisRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -35,6 +37,10 @@ class HomeController extends AbstractController
 
         $voiture = $this->getDoctrine()
                 ->getRepository(Voiture::class)
+                ->findBy([], ["modele" => "ASC"]);
+
+        $avis = $this->getDoctrine()
+                ->getRepository(Avis::class)
                 ->findAll();
     
         return $this->render('home/index.html.twig', [
@@ -42,6 +48,7 @@ class HomeController extends AbstractController
             'marques' => $marque,
             'modeles' => $modeles,
             'voitures' => $voiture,
+            'avis' => $avis,
         ]);
 
     }
@@ -49,6 +56,7 @@ class HomeController extends AbstractController
      * @Route("/home/{id}", name="home_show", methods="GET")
      */
     public function showModeles(Marque $marque): Response {
+        // dd($marque->getModeles());
         return $this->render('home/showModele.html.twig', [
             'modeles' => $marque->getModeles()
         ]);
