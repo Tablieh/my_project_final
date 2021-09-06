4720b94347607f4db40af21dd2d310e1c59602b0
Tablieh <waledo.1997@hotmail.com>
2021-09-06 22:02:06 +0200

    almost

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -102,6 +102,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
+        
         // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
 
